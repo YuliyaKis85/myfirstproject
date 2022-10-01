b78b2f430fadf54df84dd185432046f22b3012b6
YuliyaKis85 <114609759+YuliyaKis85@users.noreply.github.com>
2022-10-01 18:30:00 -0400

    done

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+import java.util.Arrays;
+
+      class Main {
+       public static void main(String[] args) {
+
+
+
+        int [] arr = {1,2,3};
+        try {
+            System.out.println(arr[4]);
+        } catch(ArrayIndexOutOfBoundsException e)     {
+            System.out.println("This is why QA Engineers always have to do boundary testing! " +
+                    "The array only has 3 values and you've requested a 4th");
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
